805da5d99a396c7f30408fecd1f65563f5bb3d72
Kelvin Alves <kalves@kopp.com.br>
2020-03-24 12:25:25 -0300

    model cadastro pessoa juridica

--- src/app/autenticacao/cadastro-pj/components/cadastrar-pj/cadastrar-pj.component.ts
+++ src/app/autenticacao/cadastro-pj/components/cadastrar-pj/cadastrar-pj.component.ts
@@ -1,5 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { Router } from '@angular/router';
+
+import { CadastroPj } from '../../models';
+
 @Component({
   selector: 'app-cadastrar-pj',
   templateUrl: './cadastrar-pj.component.html',
@@ -7,9 +13,32 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CadastrarPjComponent implements OnInit {
 
-  constructor() { }
+  form: FormGroup;
+
+  constructor(
+    private fb: FormBuilder,
+    private snackBar: MatSnackBar,
+    private router: Router
+  ) { }
 
   ngOnInit(): void {
+    this.gerarForm();
+  }
+
+  gerarForm() {
+    this.form = this.fb.group({
+      nome: ['', [Validators.required, Validators.minLength(3)]],
+      email: ['', [Validators.required]],
+      senha: ['', [Validators.required, Validators.minLength(6)]],
+      cpf: ['', [Validators.required]],
+      razaoSocial: ['', [Validators.required, Validators.minLength(5)]],
+      cnpj: ['', [Validators.required]],
+    });
+  }
+
+  cadastrarPj() {
+    const cadastroPj: CadastroPj = this.form.value;
+    alert(JSON.stringify(cadastroPj));
   }
 
 }
